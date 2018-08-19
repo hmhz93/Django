cdd2bf0370e2dcfe2db2a38d0fdf70b177e35871
hmhz93 <hmhariz93@gmail.com>
2018-08-19 11:45:34 +0100

    administration

--- crepes_bretonnes/blog/views.py
+++ crepes_bretonnes/blog/views.py
@@ -1,12 +1,13 @@
-from datetime import datetime
-from django.shortcuts import render
+# Il faut ajouter l'import get_object_or_404, attention !
+from django.shortcuts import render, get_object_or_404
+from blog.models import Article
 
-def date_actuelle(request):
-    return render(request, 'blog/date.html', {'date': datetime.now()})
+def accueil(request):
+    """ Afficher tous les articles de notre blog """
+    articles = Article.objects.all() # Nous sélectionnons tous nos articles
+    return render(request, 'blog/accueil.html', {'derniers_articles': articles})
 
 
-def addition(request, nombre1, nombre2):    
-    total = nombre1 + nombre2
-
-    # Retourne nombre1, nombre2 et la somme des deux au tpl
-    return render(request, 'blog/addition.html', locals())
+def lire(request, id, slug):
+    article = get_object_or_404(Article, id=id, slug=slug)
+    return render(request, 'blog/lire.html', {'article':article})
